50ac8f9eff9ab857d963e2a2771569b002769445
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2025-08-26 22:34:40 +0200

    Clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {
-      colors: {
-        "dark-blue": "#2A3D56",
-        "soft-pink": "#FFAAA8;",
-        "light-blue": "#E9EFF4",
-        "dark-gray": "#565656",
-        "almost-white": "#fefefe",
-      },
-      screens: {
-        xs: "400px",
-        sm: "616px",
-        md: "725px",
-      },
-    },
+    extend: {},
   },
   // eslint-disable-next-line no-undef
   plugins: [require("flowbite/plugin")],
