5be399afbbdb4e5413fcab880ee3720bad126c44
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2025-09-03 01:02:20 +0200

    Nav

--- src/components/nav/Layout/index.jsx
+++ src/components/nav/Layout/index.jsx
@@ -4,10 +4,19 @@ import { Footer } from "./Footer";
 
 export const Layout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="bg-background flex min-h-screen flex-col">
+      <a
+        href="#main-content"
+        className="focus:bg-surface sr-only focus:not-sr-only focus:absolute focus:z-50 focus:m-4 focus:rounded-md focus:px-3 focus:py-2 focus:shadow"
+      >
+        Skip to content
+      </a>
       <Header />
-      <main className="flex-grow">
-        <Outlet />
+
+      <main id="main-content" role="main" className="flex-1">
+        <div className="mx-auto max-w-screen-2xl">
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </div>
