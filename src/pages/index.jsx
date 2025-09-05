bd811ffcfae1eb49a59adbed1cfde25c0d8fe86a
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2025-09-05 20:16:56 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ export { ConfirmedBooking } from "./ConfirmedBooking";
 export { Contact } from "./Contact";
 export { Home } from "./Home";
 
-export { LogIn } from "./LogIn/index.jsx";
+export { LogIn } from "./Login";
 
 export { Profile } from "./Profile";
 export { SignUp } from "./SignUp";
